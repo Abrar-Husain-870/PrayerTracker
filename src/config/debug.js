58300f198039b77663cce228fb497f745513e49e
Abrar Husain <husainabrar870@gmail.com>
2025-08-05 23:45:44 +0530

    Update debug.js

--- src/config/debug.js
+++ src/config/debug.js
@@ -1,2 +1,2 @@
 // Global debug flag for controlling all browser logs
-export const DEBUG_LOGS_ENABLED = true; // Set to true to enable debug logs
+export const DEBUG_LOGS_ENABLED = false; // Set to true to enable debug logs
